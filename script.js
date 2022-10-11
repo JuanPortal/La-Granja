ccdac21bf86662393a75920830b1d9e8a07cc241
Portal <juanportalg@gmail.com>
2022-10-11 13:56:52 -0500

    banner to portada

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const acercaBtn = document.querySelector('.acerca-btn')
 const horarioBtn = document.querySelector('.horario-btn')
 const contactoBtn = document.querySelector('.contacto-btn')
 
-const banner = document.querySelector('.banner')
+const portada = document.querySelector('.portada')
 const acerca = document.querySelector('.acerca')
 const horario = document.querySelector('.horario')
 const contacto = document.querySelector('.contacto')
@@ -13,14 +13,14 @@ const buttons = [acercaBtn, horarioBtn, contactoBtn]
 const sections = [acerca, horario, contacto]
 
 function displaySection(section) {
-    banner.style.display = 'none'
+    portada.style.display = 'none'
     sections.forEach(section => {
         section.style.display = 'none'
     })
     section.style.display = 'flex'
 }
 
-displaySection(banner)
+displaySection(portada)
 
 function clickedButton (button) {
     buttons.forEach(button => {
@@ -30,7 +30,7 @@ function clickedButton (button) {
 }
 
 h1.addEventListener('click', () => {
-    displaySection(banner)
+    displaySection(portada)
     buttons.forEach(button => {
         button.style.textDecoration = 'none'
     })
